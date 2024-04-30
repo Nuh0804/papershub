7a93c2afbb63c79ff534e91c43b04afa51ef0b6f
Nuh0804 <nuh.saidi.buhero@gmail.com>
2024-04-30 22:44:25 +0300

    bug fixes for course with year and semester

--- papershub/learning_system/serializers.py
+++ papershub/learning_system/serializers.py
@@ -1,11 +1,7 @@
 from rest_framework import serializers
 from .models import *
 
-class YearCHoices(models.IntegerChoices):
-    first = 1
-    second = 2
-    third = 3
-    fourth = 4
+
 
 class DegreeProgramSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,13 +9,13 @@ class DegreeProgramSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+
 class CourseSerializer(serializers.ModelSerializer):
-    # degree_id = DegreeProgramSerializer(many = True, read_only = True)
-    # year_taught =serializers.ListField(child = serializers.IntegerField(), required = False, allow_empty = True)
     class Meta:
         model = Course
-        fields = ['id', 'name', 'description', 'degree_id', 'year_taught', 'semester', 'notes']
-        
+        fields = ['id', 'name',"degree", 'description', 'notes', "year", "semester"]
+
+
 
 
 class TutorialSerializer(serializers.ModelSerializer):
@@ -31,4 +27,4 @@ class TutorialSerializer(serializers.ModelSerializer):
 class PastPaperSerializer(serializers.ModelSerializer):
     class Meta:
         model = PastPaper
-        fields = ['id', 'title', 'course_id', 'file', 'solution']
+        fields = ['id', 'title', 'course', 'file', 'solution']
--- papershub/learning_system/urls.py
+++ papershub/learning_system/urls.py
@@ -1,14 +1,16 @@
 from rest_framework import routers
+from django.urls import path, include
 from rest_framework_nested.routers import NestedDefaultRouter
 from .views import *
 
 
 router = routers.DefaultRouter()
-router.register('Course', CourseViewset, basename='course')
+router.register('Course', CourseViewset, basename="Course")
 router.register('tutorial', TutorialViewset, basename='tutorial')
 router.register("program", DegreeViewset, basename="degree")
+router.register("paper", PastpaperViewset, basename="paper")
 
 paperrouter = NestedDefaultRouter(router, "Course", lookup = 'course')
 paperrouter.register("paper", PastpaperViewset, basename='paper')
-urlpatterns = router.urls + paperrouter.urls
 
+urlpatterns = router.urls + paperrouter.urls 
