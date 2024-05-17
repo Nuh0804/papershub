24985500348bfc29f314541ab86ba3ac4eb8f9b7
Nuh0804 <nuh.saidi.buhero@gmail.com>
2024-05-17 23:15:39 +0300

    configured mailersend for email interactions

--- papershub/settings/prod.py
+++ papershub/settings/prod.py
@@ -16,6 +16,7 @@ DATABASES = {
    'default': dj_database_url.config()
 }
 
+#media file conf
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
@@ -23,3 +24,11 @@ CLOUDINARY_STORAGE = {
 }
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+
+#email conf
+EMAIL_HOST = os.environ.get('MAILSENDER_SMTP_SERVER')
+EMAIL_HOST_USER = os.environ.get('MAILSENDER_SMTP_USERNAME')
+EMAIL_HOST_PASSWORD = os.environ.get('MAILSENDER_SMTP_PASSWORD')
+EMAIL_PORT = os.environ.get("MAILSENDER_SMTP_PORT")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+EMAIL_USE_TLS = True
